a8083cb5bfe4428558997232b1904fb6ede0c103
olimov Amir <olimov.amir@mail.ru>
2023-11-28 22:17:25 +0300

    v1

--- Types.cs
+++ Types.cs
@@ -8,17 +8,24 @@ namespace ReferenceType
 {
     class Types
     {
-        static void Foo()
+        static void Foo(int a)
         {
-
+            a = 5;
         }
-        static void Bar()
+        static void Bar(int[] arr)
         {
-
+            arr[0] = 25;
         }
         static void Main(string[] args)
         {
-            int b = 10;
+            int a = 10;
+            int[] arr = new int [10];
+            int[] b = new int[1];
+            Random random = new Random();
+            Bar(b);
+            Foo(a);
+            Console.WriteLine(a);
+            Console.WriteLine(b[0]);
         }
     }
 }
