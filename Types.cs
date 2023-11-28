3cf3cb42ed2c62977285fd610ee0dda17df80c5f
olimov Amir <olimov.amir@mail.ru>
2023-11-28 21:38:51 +0300

    Добавьте файлы проекта.

---
+++ Types.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceType
+{
+    class Types
+    {
+        static void Foo()
+        {
+
+        }
+        static void Bar()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            int b = 10;
+        }
+    }
+}
